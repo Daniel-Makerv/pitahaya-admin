e523787612633b2ebea5b79d26fa31c24ecf8c95
danielo <danielo@192.168.1.16>
2025-11-10 17:52:21 -0600

    migraciones de form y type for,

--- app/Exports/FormsExport.php
+++ app/Exports/FormsExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\TypeForm;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class FormsExport implements FromCollection, WithHeadings
+class FormsExport implements FromCollection, WithHeadings, WithTitle
 {
     protected $forms;
 
