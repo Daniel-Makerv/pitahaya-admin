af9982bb4826d3a8df1d7821e962b4b31c345866
danielo <danielo@192.168.1.16>
2025-11-10 23:16:00 -0600

    migraciones de form y type for,

--- app/Exports/FormsExport.php
+++ app/Exports/FormsExport.php
@@ -44,6 +44,91 @@ class FormsExport implements FromCollection, WithHeadings, WithTitle
                         'Tiempo estimado para iniciar' => $data['estimated_start_time'],
                     ];
                     break;
+                case 2:
+                    $dataBody = [
+                        'fecha' => Carbon::parse($data['fecha'])->format('d/m/Y'),
+                        'nombre' => $data['name_complete'],
+                        'Teléfono / WhatsApp' => $data['phone_contact'],
+                        'Municipio / Estado' => $data['city_state'],
+                        'Tipo de fruta' => $data['fruit_type'],
+                        'Volumen' => $data['volume'],
+                        'Frecuencia' => $data['frequency'],
+                        'Presentación' => $data['presentation'],
+                        'Calidad' => $data['quality'],
+                        'Lugar de entrega' => $data['delivery_place'],
+                        'Flete' => $data['freight'],
+                        'Transporte' => $data['transport'],
+                        'Pago' => $data['payment'],
+                        'Crédito' => $data['credit_days'],
+                        'Inicio' => $data['start_time'],
+                    ];
+                    break;
+                case 3:
+                    $dataBody = [
+                        'fecha' => Carbon::parse($data['fecha'])->format('d/m/Y'),
+                        'nombre' => $data['name_complete'],
+                        'Teléfono / WhatsApp' => $data['phone_contact'],
+                        'Municipio / Estado' => $data['municipality_state'],
+                        'Años cultivo' => $data['years_of_cultivation'],
+                        'Superficie(ha)' => $data['surface_ha'],
+                        'Plantas' => $data['plants'],
+                        'Variedades' => $data['varieties'],
+                        'Propósito' => $data['purpose'],
+                        'Tutor' => $data['tutor_type'],
+                        'Riego' => $data['irrigation_type'],
+                        'Fertilización' => $data['fertilization'],
+                        'Problemas' => $data['problems'],
+                        'Estado cultivo' => $data['crop_status'],
+                        'Producción' => $data['production_status'],
+                        'Objetivo' => $data['objective'],
+                        'Tipo asesoría' => $data['advisory_type'],
+                        'Involucramiento' => $data['involvement'],
+                        'Compromiso' => $data['commitment'],
+                        'Visita' => $data['visit'],
+                    ];
+                    break;
+                case 4:
+                    $dataBody = [
+                        'fecha' => Carbon::parse($data['fecha'])->format('d/m/Y'),
+                        'nombre' => $data['name_complete'],
+                        'Teléfono / WhatsApp' => $data['phone_contact'],
+                        'Municipio / Estado' => $data['city_state'],
+                        'Rol' => $data['role'],
+                        'Uso' => $data['use'],
+                        'Planta' => $data['has_plant'],
+                        'Proceso' => $data['process_type'],
+                        'Volumen' => $data['volume'],
+                        'Frecuencia' => $data['frequency'],
+                        'Estado fruta' => $data['fruit_state'],
+                        'Variedad' => $data['variety'],
+                        'Grado industria' => $data['industry_grade'],
+                        'Lugar entrega' => $data['delivery_place'],
+                        'Flete' => $data['freight'],
+                        'Pago' => $data['payment'],
+                        'Crédito' => $data['credit_days'],
+                        'Inicio' => $data['start_time'],
+                    ];
+                    break;
+                case 5:
+                    $dataBody = [
+                        'fecha' => Carbon::parse($data['fecha'])->format('d/m/Y'),
+                        'nombre' => $data['name_complete'],
+                        'Teléfono / WhatsApp' => $data['phone_contact'],
+                        'Municipio / Estado' => $data['municipality_state'],
+                        'Perfil' => $data['profile'],
+                        'Interés' => $data['interest'],
+                        'Contacto' => $data['contact_method'],
+                    ];
+                    break;
+                case 6:
+                    $dataBody = [
+                        'fecha' => Carbon::parse($data['fecha'])->format('d/m/Y'),
+                        'nombre' => $data['name_complete'],
+                        'Teléfono / WhatsApp' => $data['phone_contact'],
+                        'Municipio / Estado' => $data['municipality_state'],
+                        'Notas' => $data['notes'],
+                    ];
+                    break;
             }
 
             return $dataBody;
