041cd89c4748daf9df7e1e5a54a9cdb67deca9ab
daniell <daniell@MacBook-Pro-de-daniell.local>
2026-05-18 18:40:40 -0600

    fix: update FinishedStamp modal

--- database/seeders/TypeFormSeeder.php
+++ database/seeders/TypeFormSeeder.php
@@ -13,26 +13,26 @@ class TypeFormSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            [
-                "name" => "Planta / Esqueje",
-                "str"  => "plantOrEsqueaje",
-                "headers" => [
-                    'Fecha (DD/MM/AAAA)',
-                    'Nombre',
-                    'Teléfono / WhatsApp',
-                    'Municipio / Estado',
-                    'Tipo de terreno',
-                    'Superficie (ha)',
-                    'Disponibilidad de agua',
-                    '¿Zona con heladas?',
-                    '¿Tiene energía eléctrica?',
-                    '¿Cuenta con mano de obra?',
-                    'Nivel de decisión',
-                    '¿en qué cultivos tienes experiencia?',
-                    'Tiempo estimado para iniciar',
-                    'Notas',
-                ],
-            ],
+            // [
+            //     "name" => "Planta / Esqueje",
+            //     "str"  => "plantOrEsqueaje",
+            //     "headers" => [
+            //         'Fecha (DD/MM/AAAA)',
+            //         'Nombre',
+            //         'Teléfono / WhatsApp',
+            //         'Municipio / Estado',
+            //         'Tipo de terreno',
+            //         'Superficie (ha)',
+            //         'Disponibilidad de agua',
+            //         '¿Zona con heladas?',
+            //         '¿Tiene energía eléctrica?',
+            //         '¿Cuenta con mano de obra?',
+            //         'Nivel de decisión',
+            //         '¿en qué cultivos tienes experiencia?',
+            //         'Tiempo estimado para iniciar',
+            //         'Notas',
+            //     ],
+            // ],
             [
                 "name" => "Fruta (Nacional / Exportación)",
                 "str"  => "frut",
@@ -85,7 +85,7 @@ class TypeFormSeeder extends Seeder
             [
                 "name" => "Procesado (Congelado / Deshidratado)",
                 "str"  => "process",
-                "headers" =>[
+                "headers" => [
                     'Fecha',
                     'Nombre',
                     'Teléfono',
@@ -123,24 +123,162 @@ class TypeFormSeeder extends Seeder
                 ]
 
             ],
+            // [
+            //     "name" => "Solo Información",
+            //     "str"  => "info",
+            //     "headers" => [
+            //         'Fecha',
+            //         'Nombre',
+            //         'Notas',
+            //     ]
+
+            // ],
+
+            // v2
             [
-                "name" => "Solo Información",
+                "name" => "Plant Or Esqueaje",
+                "str"  => "plantOrEsqueaje",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "4.-Tipo de terreno",
+                    "Cargo/Puesto",
+                    "Superficie disponible",
+                    "Tipo de clima",
+                    "Temperatura mínima aproximada",
+                    "¿Se presentan heladas?",
+                    "Disponibilidad de agua",
+                    "¿Cuenta con sistema de riego?",
+                    "Rango de inversión disponible",
+                    "¿Le interesaría comercializar con PITAMEX?",
+                ]
+            ],
+            [
+                "name" => "Productive Alliances",
+                "str"  => "productive_alliances",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "Producción anual",
+                    "Semanas de producción",
+                    "Variedades principales",
+                    "Tipo de clima",
+                    "Grados Brix",
+                    "¿Cuenta con certificaciones?",
+                    "¿Le interesa producción limpia/libre de pesticidas?",
+                    "¿Cuenta con cadena de frío?",
+                    "¿Busca relación de largo plazo?",
+                ]
+            ],
+            [
+                "name" => "Commercial Channels",
+                "str"  => "commercial_channels",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "Área principal",
+                    "Canal principal",
+                    "Volumen requerido",
+                    "Variedades de interés",
+                    "Presentación deseada",
+                    "¿Qué certificaciones requiere?",
+                    "¿Qué documentos solicita?",
+                    "Método de pago",
+                    "Tiempo de pago",
+                    "¿Busca proveedor permanente?",
+                ]
+            ],
+            [
+                "name" => "Technical Advice",
+                "str"  => "technical_advice",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "¿Qué necesita?",
+                    "¿Cuenta con huerta establecida?",
+                    "Superficie",
+                ]
+            ],
+
+            [
+                "name" => "Processing Frozen",
+                "str"  => "processing_frozen",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "Cargo/Puesto",
+                    "¿Qué le interesa?",
+                    "Volumen requerido",
+                    "¿Requiere certificaciones?",
+                ]
+            ],
+            [
+                "name" => "Collaborations",
+                "str"  => "collaborations",
+                "headers" => [
+                    "Nombre completo",
+                    "Empresa/Rancho",
+                    "Cargo/Puesto",
+                    "Área principal",
+                    "2.-Teléfono / WhatsApp",
+                    "2.-Correo",
+                    "2.-País",
+                    "3.- Estado/Ciudad",
+                    "Tipo de colaboración",
+                    "¿Qué busca aportar?",
+                ]
+            ],
+            [
+                "name" => "Info",
                 "str"  => "info",
                 "headers" => [
-                    'Fecha',
-                    'Nombre',
-                    'Notas',
+                    "Nombre completo",
+                    "Cargo/Puesto",
+                    "¿Qué información le interesa?",
                 ]
-
-            ],
+            ]
         ];
 
         foreach ($data as $item) {
-            TypeForm::create([
-                'name' => $item['name'],
-                'str'  => $item['str'],
-                'headers' => json_encode($item['headers'], JSON_UNESCAPED_UNICODE),
-            ]);
+            TypeForm::firstOrCreate(
+                [
+                    'str' => $item['str'],
+                ],
+                [
+                    'name' => $item['name'],
+                    'headers' => json_encode($item['headers'], JSON_UNESCAPED_UNICODE),
+                ]
+            );
         }
     }
 }
